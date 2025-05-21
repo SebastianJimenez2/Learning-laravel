69dd2cd4a3c9a2a00288eebdbf050f9a11d65976
SebastianJimenez2 <alejandro.jimenez@epn.edu.ec>
2025-05-21 16:42:57 -0500

    feat: first (not literally) project on laravel

---
+++ blog/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('posts');
+});
+
+Route::get('/posts/{post}', function ($slug) {
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+    if (!file_exists($path)) {
+        return redirect('/');
+    }
+
+    $post = cache()->remember("posts.{$slug}", 5, function () use ($path) {
+        return file_get_contents(realpath($path));
+    });
+
+    return view('post', [
+        'post' => $post,
+    ]);
+})->where('post', '[A-z\-_]+');;
