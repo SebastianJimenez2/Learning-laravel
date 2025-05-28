04965688b58881424112d2a9e8de6f31176c7f8c
SebastianJimenez2 <alejandro.jimenez@epn.edu.ec>
2025-05-28 16:58:05 -0500

    feat: pagination and author filtering

--- blog/database/seeders/DatabaseSeeder.php
+++ blog/database/seeders/DatabaseSeeder.php
@@ -16,6 +16,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Post::factory(5)->create();
+        Post::factory(30)->create();
     }
 }
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,27 +1,8 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Models\Category;
-use App\Models\User;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
-use App\Models\Post;
 
 
 Route::get('/', [PostController::class, 'index']);
-
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
-
-Route::get('categories/{category:slug}', function (Category $category) {
-    return view('posts', [
-        'posts' => $category->posts,
-        'categories' => Category::all(),
-    ]);
-});
-
-Route::get('authors/{author:username}', function (User $author) {
-    return view('posts', [
-        'posts' => $author->posts,
-        'categories' => Category::all(),
-    ]);
-});
