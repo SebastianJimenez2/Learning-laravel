460a0868cfb319a5c306c6af6ce62b72f0bfc785
SebastianJimenez2 <alejandro.jimenez@epn.edu.ec>
2025-05-29 12:15:06 -0500

    feat: whole login logic and begining of comments for a post

--- blog/app/Models/User.php
+++ blog/app/Models/User.php
@@ -17,11 +17,7 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -44,6 +40,11 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
     }
 
     public function posts()
