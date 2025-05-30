1f545116846d988043562b75290626b12b667cf3
SebastianJimenez2 <alejandro.jimenez@epn.edu.ec>
2025-05-30 12:31:44 -0500

    feat: frontend improved

--- blog/resources/views/components/layout.blade.php
+++ blog/resources/views/components/layout.blade.php
@@ -35,16 +35,36 @@
 
         <div class="mt-8 md:mt-0 flex items-center">
             @auth
-                <span class="text-xs font-bold uppercase">Welcome, {{ auth()->user()->name }}!</span>
+                <x-dropdown>
+                    <x-slot name="trigger">
+                        <button class="text-xs font-bold uppercase">Welcome, {{ auth()->user()->name }}!</button>
+                    </x-slot>
 
-                <form method="POST" action="/logout" class="text-xs font-semibold text-blue-500 ml-6">
-                    @csrf
+                    <x-dropdown-item
+                        href="/admin/dashboard">Dashboard
+                    </x-dropdown-item>
 
-                    <button type="submit">Log Out</button>
-                </form>
+                    <x-dropdown-item
+                        href="/admin/posts/create"
+                        :active="request()->is('admin/posts/create')">New Post
+                    </x-dropdown-item>
+
+                    <x-dropdown-item
+                        href="#" x-data="{}"
+                        @click.prevent="document.querySelector('#logout-form').submit()">Log Out
+                    </x-dropdown-item>
+
+                    <form id="logout-form" method="POST" action="/logout" class="hidden">
+                        @csrf
+                    </form>
+                </x-dropdown>
             @else
-                <a href="/register" class="text-xs font-bold uppercase">Register</a>
-                <a href="/login" class="ml-6 text-xs font-bold uppercase">Log In</a>
+                <a href="/register"
+                   class="text-xs font-bold uppercase {{ request()->is('register') ? 'text-blue-500' : '' }}">Register</a>
+                Add commentMore actions
+                <a href="/login"
+                   class="ml-6 text-xs font-bold uppercase {{ request()->is('login') ? 'text-blue-500' : '' }}">Log
+                    In</a>
             @endauth
 
             <a
@@ -80,7 +100,7 @@
                                 class="lg:bg-transparent py-2 lg:py-0 pl-4 focus-within:outline-none">
 
                             @error('email')
-                                <span class="text-red-500 text-xs mt-2">{{ $message }}</span>
+                            <span class="text-red-500 text-xs mt-2">{{ $message }}</span>
                             @enderror
                         </div>
                     </div>
